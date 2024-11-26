39ae69670e12fa8661871c4cb797391b792f31af
iamrayo <aderayoadegbenro778@gmail.com>
2024-11-26 13:36:39 +0100

    Changes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', function(event) {
     const error = document.getElementById(field + 'Error');
     
     if (!input.value.trim()) {
-      error.textContent = ${field.replace(/([A-Z])/g, ' $1')} is required.;
+      error.textContent = '${field.replace(/([A-Z])/g, "$1")} is required';
       valid = false;
     } else {
       error.textContent = '';
@@ -22,3 +22,25 @@ form.addEventListener('submit', function(event) {
     form.reset();
   }
 });
+
+const apiUrl3 = "https://loanmangementbackends.onrender.com/api/loanapplication ";
+
+async function Loan() {
+    const response = await fetch(apiUrl3, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(FormData)
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+        if (details.success) {
+            alert("Details captured successfully!");
+            window.location.href = "loan request.html";
+        } else {
+            alert("Invalid Creditials, Try again!");
+         } 
+        }
+} 
